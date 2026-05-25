4d9a769ac12603889f6f1f1ad2b3fd893d7a4d68
ritikaguleria <guleria.ritika@tftus.com>
2026-05-25 13:40:42 +0530

    fixed env

--- test-options.ts
+++ test-options.ts
@@ -10,7 +10,8 @@ export const test = base.extend<TestOptions>({
 
   formLayoutPage: async ({ page }, use) => {
     // await page.goto("/");
-    await page.goto(process.env.URLOfWebApp);
+    // await page.goto(process.env.URLOfWebApp);
+    await page.goto("http://localhost:4200/");
     await page.getByText("Forms").click();
     await page.getByText("Form Layouts").click();
     await use("");
